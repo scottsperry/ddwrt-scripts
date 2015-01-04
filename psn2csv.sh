711410172c38290194503db35aaaf73bad31ec44
user <user>
2015-01-03 22:52:28 -0500

    do both input and output

--- psn2csv.sh
+++ psn2csv.sh
@@ -5,17 +5,21 @@ interface=eth0
 if [ $# -eq 1 ]; then
     interface=$1;
 fi
-output="bytesout.${interface}.csv"
+output_out="bytesout.${interface}.csv"
+output_in="bytesin.${interface}.csv"
 
-
-echo 'date,value' >$output
+echo 'date,value' >$output_out
+echo 'date,value' >$output_in
 for i in $datafiles 
  do 
  IFS='_' read -a array <<< "$i"
  thedate="${array[-1]}"
- echo -n "$thedate," >>$output
-# ./parse_psn1.sh $i $interface >>$output
+ echo -n "$thedate," >>$output_out
+ echo -n "$thedate," >>$output_in
+ 
+ awk "/^ *${interface}:/"' { if ($1 ~ /.*:[0-9][0-9]*/) { sub(/^.*:/, "") ; print $1 } else { print $2 } }' $i >> $output_in
+ awk "/^ *${interface}:/"' { if ($1 ~ /.*:[0-9][0-9]* /) { print $9 } else { print $10 } }' $i >> $output_out
 
- awk "/^ *${interface}:/"' { if ($1 ~ /.*:[0-9][0-9]*/) { sub(/^.*:/, "") ; print $1 } else { print $2 } }' $i >> $output
+
 done
 
